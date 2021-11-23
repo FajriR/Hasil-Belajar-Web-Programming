96d49b3d9a7d9f844706fe1cd904d171b3e31a61
fajriramadhan.github.io <fajriramadhan9980799856@gamil.com>
2021-11-23 21:10:02 +0700

    .

---
+++ Back End Web Developtment/02.kuliah pemograman web 2020/04.searching data/functions.php
@@ -0,0 +1,105 @@
+<?php
+
+// jika tidak ada id di url
+// if (!isset($_GET['id'])) {
+//   header("Location:index.php");
+//   exit;
+// }
+
+function koneksi()
+{
+  return mysqli_connect('localhost', 'root', '', 'phpdasar');
+}
+
+function query($query)
+{
+  $db = koneksi();
+  $result = mysqli_query($db, $query);
+
+  // jika hasilnya hanya 1 data
+  if (mysqli_num_rows($result) == 1) {
+    return mysqli_fetch_assoc($result);
+  }
+
+  $rows = [];
+  while ($row = mysqli_fetch_assoc($result)) {
+    $rows[] = $row;
+  }
+  return $rows;
+}
+
+
+
+function tambah($data)
+{
+  $db = koneksi();
+
+  $nama = htmlspecialchars($data['nama']);
+  $asal = htmlspecialchars($data['asal']);
+  $email = htmlspecialchars($data['email']);
+  $jurusan = htmlspecialchars($data['jurusan']);
+  $gambar = htmlspecialchars($data['gambar']);
+
+  $query = "INSERT INTO 
+           mahasiswa
+           VALUES
+           (null, '$nama','$asal','$email','$jurusan',$gambar);
+           ";
+  mysqli_query($db, $query) or die(mysqli_error($db));
+  return mysqli_affected_rows($db);
+}
+
+function hapus($id)
+{
+  $db = koneksi();
+  mysqli_query($db, "DELETE FROM mahasiswa WHERE id = $id") or die(mysqli_error($db));
+  return mysqli_affected_rows($db);
+}
+
+
+
+function ubah($data)
+{
+  $db = koneksi();
+
+  $id = $data['id'];
+
+  $nama = htmlspecialchars($data['nama']);
+  $asal = htmlspecialchars($data['asal']);
+  $email = htmlspecialchars($data['email']);
+  $jurusan = htmlspecialchars($data['jurusan']);
+  $gambar = htmlspecialchars($data['gambar']);
+
+  $query = "UPDATE mahasiswa SET 
+           nama ='$nama',
+           asal ='$asal',
+           email ='$email',
+           jurusan ='$jurusan',
+           gambar = '$gambar'
+           WHERE id = $id";
+
+  mysqli_query($db, $query) or die(mysqli_error($db));
+  return mysqli_affected_rows($db);
+}
+
+
+
+function cari($keyword)
+{
+  $db = koneksi();
+
+  $query = "SELECT * FROM mahasiswa WHERE 
+           nama LIKE '%$keyword%' OR
+           asal LIKE '%$keyword%' OR
+           email LIKE '%$keyword%' OR
+           jurusan LIKE '%$keyword%'
+           ";
+
+  $result = mysqli_query($db, $query);
+  $rows = [];
+
+  while ($row = mysqli_fetch_assoc($result)) {
+    $rows[] = $row;
+  }
+  return $rows;
+}
---
+++ Back End Web Developtment/02.kuliah pemograman web 2020/04.searching data/ubah.php
@@ -0,0 +1,86 @@
+<?php
+require 'functions.php';
+
+// jika tidak ada id di url
+if (!isset($_GET['id'])) {
+  header("Location: index.php");
+  exit;
+}
+
+// ambil id dari url
+$id = $_GET['id'];
+
+// query mahasiswa berdasarkan id
+$m = query("SELECT * FROM mahasiswa WHERE id = $id");
+
+// cek apakah tombol ubah sudah di tekan
+if (isset($_POST["ubah"])) {
+
+  if (ubah($_POST) > 0) {
+    echo "<script>
+          alert('data berhasil di ubah');
+          document.location.href = 'index.php';
+          </script>
+          ";
+  } else {
+    echo "data gagal di ubah";
+  }
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Ubah Data Mahasiswa</title>
+</head>
+
+<body>
+
+  <h3>Form Ubah Data Mahasiswa</h3>
+  <form action="" method="POST">
+    <input type="hidden" name="id" value="<?= $m['id']; ?>">
+    <ul>
+      <li>
+        <label>
+          Nama :
+          <input type="text" name="nama" autofokus required value="<?= $m['nama']; ?>">
+        </label>
+      </li>
+      <li>
+        <label>
+          Asal :
+          <input type="text" name="asal" required value="<?= $m['asal']; ?>">
+        </label>
+      </li>
+      <li>
+        <label>
+          Email :
+          <input type="text" name="email" required value="<?= $m['email']; ?>">
+        </label>
+      </li>
+      <li>
+        <label>
+          Jurusan :
+          <input type="text" name="jurusan" required value="<?= $m['jurusan']; ?>">
+        </label>
+      </li>
+      <li>
+        <label>
+          Gambar :
+          <input type="text" name="gambar" required value="<?= $m['gambar']; ?>">
+        </label>
+      </li>
+      <li>
+        <button type="submit" name="ubah">Ubah Data</button>
+      </li>
+    </ul>
+  </form>
+
+
+</body>
+
+</html>
